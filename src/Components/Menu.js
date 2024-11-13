247ab43185924cb72cbb4461b34a7bdccd80ba44
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-13 19:07:31 +0000

    started woirking on menu look

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -5,22 +5,20 @@ import Link from "next/link"
 
 export default function Menu () {
 
-return (<div className="w-full bg-red-500 text-white absolute left-[-100%] top-[200px]" id="menu">
+return (<div className="w-full  text-slate-800 font-bold absolute left-[-100%] top-[200px]" id="menu">
 
 
 
 
 
 
-<Link href="/" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-1/7">HOME </Link>
-
-<Link href="/About" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-9">ABOUT </Link>
-
-
-<Link href="/Contact" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-9">CONTACT </Link>
-<Link href="/Print" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-9">  PRINT DESIGN </Link>
-<Link href="/Web" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-9">  WEB DESIGN </Link>
-<Link href="/Video" className="flex-1 bg-blue-500 m-4 flex justify-center items-center min-w-9">  VIDEO EDITING </Link>
+<Link href="/" className="flex-1 bg-pink-300 m-4 flex justify-center items-center min-w-1/7 py-7 rounded shadow-2xl">Home </Link>
+<Link href="/About" className="flex-1 bg-pink-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">About </Link>
+<Link href="/Web" className="flex-1 bg-green-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">  UI / UX Design </Link>
+<Link href="/Digital" className="flex-1 bg-green-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">  Digital Design Editing </Link>
+<Link href="/Video" className="flex-1 bg-green-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">  Video Editing </Link>
+<Link href="/Print" className="flex-1 bg-green-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">  Print Design</Link>
+<Link href="/Contact" className="flex-1 bg-pink-300 m-4 flex justify-center items-center min-w-9 py-7 rounded shadow-2xl">Contact </Link>
 
 
 
