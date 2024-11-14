224072d3e1fe4261f6b02ae293ef1725caf420de
Lucy de Rojas <lucyderojas@gmail.com>
2024-11-14 23:00:58 +0000

    finished about and contact pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,34 @@
 import Layout from "@/Components/Layout"
-
+import Image from "next/image";
+import Head from "next/head";
 
 
 export default function About () {
 
 return (<Layout>
+<main className="px-14 ">
+
+
 <h1>About</h1>
+<div className="flex justify-center">
+
+<Image
+ src="/Media/Images/Lucy_de_Rojas_Profile_Illustration.png"
+ width={200}
+ height={200}
+ alt="Lucy de Rojas Profile Illustration"
+
+/>
+ </div>
+
+<br />
+<p>My name is Lucy and I’m a digital, web, and graphic designer with experience in video editing, combining creativity with technical skills to create visually engaging designs across different platforms.
+<br /><br />
+My background in professional athletics has helped me develop discipline and focus, while my love for puzzles has strengthened my problem-solving abilities, which I apply to my design work in a thoughtful and strategic way.</p>
+
+<br />
+Ready to design with purpose? Let’s shape visuals that turn first impressions into lasting connections.
 
 
+</main>
 </Layout>)};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from "@/Components/Layout";
+import Head from "next/head";
 
 
 
@@ -14,10 +15,35 @@ export default function Home() {
 
 
   return (<Layout>
+    <Head>
+      <title>Welcome to Pages of Lucy de Rojas - Digital, Web and Print Designer, Plus Video Editor </title>
+    </Head>
 
-    <h1>hello tailwind</h1>
+
+<main className= "px-14">
+    <h1>Welcome! </h1>
 
 
+
+    <p>My name is Lucy de Rojas and I’m UI, UX, digital & graphic designer.
+
+
+
+Pleased to have you visiting my pages. I'm a seasoned digital creator with a humble 20 years of experience tucked under my creative belt. I take immense pleasure in collaborating with a diverse array of clients, ranging from the corporate industry titans to the local gems.
+<br />
+<br />
+Over the years, my creative journey has entwined with household names such as Bosch, Nokia, Nike, TK Maxx, Readers Digest, NHS, Boden, and Homebase. Working with these esteemed names has not only honed my skills but has also fueled my passion for creating designs that resonate with diverse audiences.
+
+<br/>
+<br/>
+Up for a conversation about infusing some design enchantment into your project? How about a friendly chat over a cuppa to explore how I can breathe life into your ideas?
+<br />
+<br />
+Feel free to reach out, and let's dive into this creative journey together. I'm eager to craft something exceptional with you! Additionally, I'm here to contribute to the growth of your business through my skills and extensive experience.
+</p>
+
+
+</main>
 
 
 
